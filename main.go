ecf0520408240fdd1f6a809a6122669729e354b0
sinmetal <metal.tie@gmail.com>
2018-10-24 18:02:49 +0900

    backendhellotime serviceにrequestを送る処理を追加

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"time"
 )
 
 func main() {
 	for {
-		fmt.Printf("Hello %s\n", time.Now())
+		res, err := http.Get("http://backendhellotime-service.default.svc.cluster.local:8080")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer res.Body.Close()
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(b))
+
 		time.Sleep(5 * time.Second)
 	}
 }
