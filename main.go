04d5826f6b743d4e22935db0660a6092756e8d3a
sinmetal <metal.tie@gmail.com>
2018-10-25 17:55:53 +0900

    Stackdriver Traceのサンプルを追加

--- main.go
+++ main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
+
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"go.opencensus.io/trace"
 )
 
 func main() {
+	exporter, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: "souzoh-demo-gcp-001",
+	})
+	if err != nil {
+		panic(err)
+	}
+	trace.RegisterExporter(exporter)
+
 	for {
-		res, err := http.Get("http://backendhellotime-service.default.svc.cluster.local:8080")
+		ctx := context.Background()
+		msg, err := process(ctx)
 		if err != nil {
 			fmt.Println(err)
 		}
-		defer res.Body.Close()
-		b, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(string(b))
+		fmt.Println(msg)
 
 		time.Sleep(5 * time.Second)
 	}
+}
+
+func process(ctx context.Context) (string, error) {
+	ctx, span := trace.StartSpan(ctx, "/process")
+	defer span.End()
+
+	res, err := http.Get("http://backendhellotime-service.default.svc.cluster.local:8080")
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
